cf48c686cd0d8ea3bdcce0d192809b4e56a53407
北桥 <tonghao1222@qq.com>
2021-09-10 18:18:14 +0800

    部分修改

--- docs/.vuepress/components/qqGroup.vue
+++ docs/.vuepress/components/qqGroup.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="qq-group">
-        <div class="qq-group__item">官方1群： 1042987248(已满)</div>
-        <div class="qq-group__item">官方2群： 249718512(已满)</div>
-        <div class="qq-group__item">官方3群： 1129077272(已满)</div>
-        <div class="qq-group__item">官方4群： 1084514613(已满)</div>
-        <div class="qq-group__item">
-             官方5群： 863820668(已满)
-        </div>
-        <div class="qq-group__item">
-            官方6群：745721078(已满)
-        </div>
-         <div class="qq-group__item">
-            官方7群：
-            <a href="https://jq.qq.com/?_wv=1027&k=ZHFDsWUV" target="_blank">627867855</a>
-        </div>
+  <div class="qq-group">
+    <div class="qq-group__item">官方1群： 1042987248(已满)</div>
+    <div class="qq-group__item">官方2群： 249718512(已满)</div>
+    <div class="qq-group__item">官方3群： 1129077272(已满)</div>
+    <div class="qq-group__item">官方4群： 1084514613(已满)</div>
+    <div class="qq-group__item">官方5群： 863820668(已满)</div>
+    <div class="qq-group__item">官方6群：745721078(已满)</div>
+    <div class="qq-group__item">官方7群：627867855(已满)</div>
+    <div class="qq-group__item">官方8群：496409492(已满)</div>
+
+    <div class="qq-group__item">
+      官方9群：
+      <a href="https://jq.qq.com/?_wv=1027&k=Ute1wecV" target="_blank">828504448</a>
     </div>
+  </div>
 </template>
 
 <script>
@@ -22,10 +21,10 @@
 
 <style lang="scss" scoped>
 .qq-group {
-    &__item {
-        margin-top: 12px;
-        font-size: 14px;
-        color: #5e6d82;
-    }
+  &__item {
+    margin-top: 12px;
+    font-size: 14px;
+    color: #5e6d82;
+  }
 }
 </style>
