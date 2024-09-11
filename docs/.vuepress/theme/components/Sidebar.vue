1c924c323825a370f8fde07d16ed11f720d3d832
yiruiwen <yiruiwen@uviewui.com>
2024-09-11 12:55:26 +0800

    docs: update doc.

--- docs/.vuepress/theme/components/Sidebar.vue
+++ docs/.vuepress/theme/components/Sidebar.vue
@@ -23,7 +23,7 @@
             <a
                 target="_blank"
                 class="jump-linker"
-                href="https://www.upyun.com/?from=uview"
+                href="https://www.houdeyun.cn/?from=uview"
             >
                 <img src="https://cdn.uviewui.com/uview/resources/b490bd2cefd8a827cbfed7d6386b368a.PNG" />
             </a>
